ccefa810dacb825f784fe0a6812cd9cb6949e4cf
Levon Nalbandyan <levon.nalb@gmail.com>
2017-07-14 18:56:14 +0400

    Foru pages done

--- app/pages/forum-home/forum-home.component.js
+++ app/pages/forum-home/forum-home.component.js
@@ -7,33 +7,9 @@ component('forumHome', {
     controllerAs: 'vm',
     controller: function forumHomeComponenet($uibModal ) {
         var vm = this;
-        if ($(window).width() < 1025) {
+        var $sticky = $('.sticky');
+        $sticky.css({position: 'fixed', top: '66px'});
 
-        } else {
-            var $sticky = $('.sticky');
-            var $stickyrStopper = $('.sticky-stopper');
-            if (!!$sticky.offset()) { // make sure ".sticky" element exists
-
-                var generalSidebarHeight = $sticky.innerHeight();
-                var stickyTop = $sticky.offset().top;
-                var stickOffset = 0;
-                var stickyStopperPosition = $stickyrStopper.offset();
-                var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
-                var diff = stopPoint + stickOffset;
-
-                $(window).scroll(function () { // scroll event
-                    var windowTop = $(window).scrollTop(); // returns number
-
-                    if (stopPoint < windowTop) {
-                        $sticky.css({position: 'absolute', top: diff});
-                    } else if (stickyTop < windowTop + stickOffset) {
-                        $sticky.css({position: 'fixed', top: '77px'});
-                    } else {
-                        $sticky.css({position: 'absolute', top: '0px'});
-                    }
-                });
-            }
-        }
         vm.openReport = function (size) {
             var parentElem = angular.element('body');
             var modalInstance = $uibModal.open({
