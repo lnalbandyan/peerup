488fa2eb40f35cbb8ed6eadf474a2efaafb4c684
Levon Nalbandyan <levon.nalb@gmail.com>
2017-08-04 01:03:11 +0400

    Fixes

--- app/pages/group-feed/group-feed.component.js
+++ app/pages/group-feed/group-feed.component.js
@@ -8,33 +8,17 @@ component('groupFeed', {
     controller: function groupFeedComponenet($uibModal) {
         var vm = this;
 
-        if ($(window).width() < 1025) {
+        if ($(window).width() > 1025) {
 
-        } else {
             var $sticky = $('.sticky');
-            var $stickyrStopper = $('.sticky-stopper');
-            if (!!$sticky.offset()) { // make sure ".sticky" element exists
-
-                var generalSidebarHeight = $sticky.innerHeight();
-                var stickyTop = $sticky.offset().top;
-                var stickOffset = 0;
-                var stickyStopperPosition = $stickyrStopper.offset();
-                var stopPoint = stickyStopperPosition - generalSidebarHeight - stickOffset;
-                var diff = stopPoint + stickOffset;
-
-                $(window).scroll(function () { // scroll event
-                    var windowTop = $(window).scrollTop(); // returns number
-
-                    if (stopPoint < windowTop) {
-                        $sticky.css({position: 'absolute', top: diff});
-                    } else if (stickyTop < windowTop + stickOffset) {
-                        $sticky.css({position: 'fixed', top: '77px'});
-                    } else {
-                        $sticky.css({position: 'absolute', top: '0px'});
-                    }
-                });
-            }
+            $sticky.css({position: 'fixed', top: '86px'});
         }
+
+        vm.addKeyword = function (e) {
+            console.log(e);
+            $(e.target).hide();
+            $(e.target).parent().next('div').addClass('active');
+        };
 
         vm.postLink = function (e) {
             $(".create-poll, .brain-map, .ask-question, .share-story, .guest-list").hide();
